5d3c8a493268ecd6f1673bb7fed9520762cf328f
Reynaldo Teixeira Jr <reynaldoteixeira@gmail.com>
2023-09-07 16:57:27 -0300

    Clean code

--- src/app/tracker-page/tracker-page.component.ts
+++ src/app/tracker-page/tracker-page.component.ts
@@ -2,42 +2,41 @@ import { AfterViewInit, Component, OnInit } from "@angular/core";
 import * as L from 'leaflet';
 
 @Component({
-    selector:'app-track-page',
-    templateUrl:'tracker-page.component.html',
-    styleUrls:['tracker-page.component.scss']
+  selector: 'app-track-page',
+  templateUrl: 'tracker-page.component.html',
+  styleUrls: ['tracker-page.component.scss']
 })
-export class TrackerPageComponent implements OnInit, AfterViewInit{
-    private map!: L.Map
+export class TrackerPageComponent implements OnInit, AfterViewInit {
+  private map!: L.Map
 
-    markers: L.Marker[] = [
-      L.marker([-23.533773,  -46.625290]), // SP
-    ];
+  markers: L.Marker[] = [
+    L.marker([-23.533773, -46.625290]), // SP
+  ];
 
-    constructor(){}
+  constructor() { }
 
-    ngOnInit(): void {
-        this.initializeMap();
-        this.addMarkers();
-        this.centerMap();
-    }
+  ngOnInit(): void {
+    this.initializeMap();
+    this.addMarkers();
+    this.centerMap();
+  }
 
-    ngAfterViewInit(): void {}
-        
+  ngAfterViewInit(): void { }
 
-    private initializeMap() {
-        const baseMapURl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        this.map = L.map('map');
-        L.tileLayer(baseMapURl).addTo(this.map);
-        
-      }
-    
-    
-      private addMarkers() {
-        this.markers.forEach(marker => marker.addTo(this.map));
-      }
-    
-      private centerMap() {
-        const bounds = L.latLngBounds(this.markers.map(marker => marker.getLatLng()));
-        this.map.fitBounds(bounds);
-      }
+
+  private initializeMap() {
+    const baseMapURl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    this.map = L.map('map');
+    L.tileLayer(baseMapURl).addTo(this.map);
+
+  }
+
+  private addMarkers() {
+    this.markers.forEach(marker => marker.addTo(this.map));
+  }
+
+  private centerMap() {
+    const bounds = L.latLngBounds(this.markers.map(marker => marker.getLatLng()));
+    this.map.fitBounds(bounds);
+  }
 }
