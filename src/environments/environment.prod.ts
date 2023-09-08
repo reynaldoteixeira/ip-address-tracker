fabb5ee10ebe666bb7417b97d40dfe640361bd00
Reynaldo Teixeira Jr <reynaldoteixeira@gmail.com>
2023-09-08 19:21:39 -0300

    Remove enviroments variables to config and update npm

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  ipGeoApiKey: process.env.ipGeoApiKey
+  ipGeoApiKey: ""
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  ipGeoApiKey: process.env.ipGeoApiKey
+  ipGeoApiKey: ""
 };
 
 /*
